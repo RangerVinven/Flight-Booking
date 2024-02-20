b9729903a3b0c23deaab7424ac585b9681bf2fa1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-20 12:05:35 +0000

    Changed the port to 3307

--- code/utils/database_connector.py
+++ code/utils/database_connector.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
-db = mysql.connector.connect(user="root", password="root", host="127.0.0.1", database="FlightBooking")
+db = mysql.connector.connect(user="root", password="root", host="127.0.0.1", port="3307", database="FlightBooking")
 cursor = db.cursor(dictionary=True)
